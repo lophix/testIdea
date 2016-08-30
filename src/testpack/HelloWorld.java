ac775a2ffb830e16101e27e7ffc8a63c693a4280
xujian <xujian_xtkf@win-sky.com.cn>
2016-08-30 19:57:40 +0800

    Initial commit

---
+++ src/testpack/HelloWorld.java
@@ -0,0 +1,11 @@
+package testpack;
+
+/**
+ * Created by Administrator on 2016/8/30.
+ */
+public class HelloWorld {
+    public  static void main(String[] args){
+        System.out.println("hello there");
+    }
+
+}
